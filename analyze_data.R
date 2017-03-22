00f164a5c7380d443047121b8ab84db9393c9994
poldrack <poldrack@gmail.com>
2017-03-22 14:02:11 -0700

    fix datafile address

---
+++ analyze_data.R
@@ -0,0 +1,6 @@
+# load data and analyze relation between age and discount rate
+# after https://neurohackweek.github.io/software-testing-for-scientists/02-example_script/
+
+df=read.table('http://poldrack.github.io/reproducible-workflows/testdata.csv')
+
+# add assertion tests
